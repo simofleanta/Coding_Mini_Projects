5fa1e35b76803730d5356f92b209b03702bc81c9
simofleanta <simo.fleanta@gmail.com>
2020-10-03 13:40:24 +0200

    changes

---
+++ math_function.py
@@ -0,0 +1,20 @@
+"""given integer inva two integer number retun their product and 
+if the product is greater than 1000 reurn their sum"""
+
+
+def math(a,b):
+    if 600>1000:
+        return a+b
+    else:
+        return a*b
+print(math(20,30))
+
+
+
+def f(c,d):
+    if 600<1000:
+        return c+d
+    else:
+        return c*d
+print(f(30,40))
+
