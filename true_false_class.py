9a3e27582aacd5f20b4b84a412e447fa04a7f9ab
simofleanta <simo.fleanta@gmail.com>
2020-10-05 17:33:54 +0200

    save on git

--- true_false_class.py
+++ true_false_class.py
@@ -35,6 +35,54 @@ class z(y):
     print(b.f(-3))
 
 
+"""which of these expressions are true?
+12-3<3
+9-7!=3
+"""
+
+class bul:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def statement(self,a,b):
+        if a-b!=3:
+            return False 
+        else:
+            return bool
+
+s=bul(9,7)
+print(s.statement(9,7))
+
+class ex(bul):
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def c(self,a,b):
+        if a-b<10:
+            return False 
+        else:
+            return bool
+
+d=ex(12,3)
+print(d.c(12,3))
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
 
