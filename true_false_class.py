37aca93787ab3b2dccba3925fb04e8d10fa2e352
simofleanta <simo.fleanta@gmail.com>
2020-10-05 16:49:30 +0200

    git push

--- true_false_class.py
+++ true_false_class.py
@@ -3,13 +3,23 @@ class x:
         self.x=x
     
     def f(self,x):
-        return 5+x<8
+        return 17-x>=11
 
 p=x(-5)
 
 print(p.f(-5))
 
 class y(x):
-    pass
+    def __init__(self,x):
+        self.x=x
+    
+    def f(self,x):
+        return 24-x>20
+
+a=x(10)
+print(a.f(1))
+print(a.f(11))
+
+
 
 
