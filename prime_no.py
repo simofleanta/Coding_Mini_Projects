58b44796941e2e0d5f8a196f42c90079f89db039
simofleanta <simo.fleanta@gmail.com>
2020-10-05 18:58:25 +0200

    save it

--- prime_no.py
+++ prime_no.py
@@ -8,3 +8,29 @@ def is_prime(num, primes=(2)):
 		return str(primes) in str(num)
 print(is_prime([2, 3, 5, 7, 11, 13]))
 
+
+"""dictionary exercise 7: Check if a value 200 exists in a dictionary"""
+def is_there(num, dict=(200)):
+	return str(dict) in str(num)
+print (is_there({'a': 100, 'b': 200, 'c': 300}))
+
+"""make it a class"""
+
+class x:
+	def __init__(self, num, sample_d):
+		self.num=num
+		self.sample_d=sample_d
+	
+	def d(self,num,sample_d):
+		return str(sample_d) in str(num)
+	
+	result=x({'a': 100, 'b': 200, 'c': 300})
+	print(result.d())
+
+
+
+
+
+
+
+
