c04c497f4eb544c1140fdeb0adee70bf08b495b7
simofleanta <simo.fleanta@gmail.com>
2020-10-05 20:00:42 +0200

    save it

---
+++ turtle.py
@@ -0,0 +1,22 @@
+""" draw stuff"""
+
+import turtle
+
+def Turtle():
+    for i in range (4):
+        t.forward(100)
+        t.left(90)
+        t.shape('square')
+        t.shape('Blue')
+        t.speed(3)
+
+t=t.Turtle()
+
+
+
+
+
+
+
+
+
