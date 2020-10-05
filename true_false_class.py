40d3953e7cb81e2aa6f816ef5aa36cf7fa552189
simofleanta <simo.fleanta@gmail.com>
2020-10-05 17:04:19 +0200

    saving to git

--- true_false_class.py
+++ true_false_class.py
@@ -1,3 +1,6 @@
+""" find x values for the following situ:"""
+#creating inherited class to chain 2 exercises expressions 
+
 class x:
     def __init__(self,x):
         self.x=x
@@ -19,6 +22,24 @@ class y(x):
 a=x(10)
 print(a.f(1))
 print(a.f(11))
+
+class z(y):
+    def __init__(self,x):
+        self.x=x
+    
+    def f(self,x):
+        return x+28<=31
+    
+    b=y(100)
+    print(b.f(100))
+    print(b.f(-3))
+
+
+
+
+
+
+
 
 
 
