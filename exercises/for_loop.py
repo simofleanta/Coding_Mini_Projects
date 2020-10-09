d041437c08b6557cd6f598d888647619ecd6f783
simofleanta <simo.fleanta@gmail.com>
2020-10-09 09:42:14 +0200

    save

---
+++ exercises/for_loop.py
@@ -0,0 +1,10 @@
+poem_lst = ["Not enjoyment, and not sorrow,", "Is our destined end or way;", "But to act, that each tomorrow",
+            "Find us farther than today."]
+
+#var
+for i in poem_lst:
+    print(i)
+
+
+
+  
