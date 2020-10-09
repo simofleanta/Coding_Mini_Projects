30169448eee15350329d7417e928387b282bb14d
simofleanta <simo.fleanta@gmail.com>
2020-10-09 11:57:54 +0200

    s

--- exercises/functions.py
+++ exercises/functions.py
@@ -1,3 +1,17 @@
 def calculation(a, b):
     return a+b
 print(calculation(40,10))
+
+
+"""Create a function showEmployee() in such a way that it should 
+accept employee name, and it’s salary and display both, and if the salary is missing in
+ function call it should show it as 9000"""
+
+ #default functions
+
+def showEmployee(name, salary=9000):
+    salary=9000
+
+    return f"Ben earns {salary}"
+
+print(showEmployee('Ben'))
