bc00b6f7cc25fecac30afebb877a5da0620bb4b6
simofleanta <simo.fleanta@gmail.com>
2020-10-09 12:12:25 +0200

    s

---
+++ exercises/function&oop.py
@@ -0,0 +1,8 @@
+"""Write a function calculation() such that it can accept two variables and calculate the 
+addition and subtraction of it. And also it must return both addition and subtraction in a single return call
+#may be try this with function in class"""
+
+def calculation(a, b):
+    return a+b
+print(calculation(40,10))
+
