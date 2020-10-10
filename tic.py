bb5c839b3f94fe1cac8c8cdde325976628793d31
simofleanta <simo.fleanta@gmail.com>
2020-10-10 08:18:21 +0200

    s

---
+++ tic.py
@@ -0,0 +1,33 @@
+import turtle
+
+board=turtle.Screen()
+t=turtle.Turtle()
+t.color("Green")
+t.width()
+t.speed(2)
+
+for i in range(4):
+    t.forward(300)
+    t.left(90)
+
+#inner lines in the square
+t.penup()
+t.goto(0,100)
+t.pendown()
+
+t.forward(300)
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.forward(300)
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.left(90)
+t.forward(300)
+t.penup()
+t.goto(200,0)
+t.pendown()
+t.forward(300)
+
+
