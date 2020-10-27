5261a95e54d757759e318ff97d6a48bbca2ba442
simofleanta <simo.fleanta@gmail.com>
2020-10-27 09:19:55 +0100

    save

---
+++ exercises/calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    """this f adds"""
+    return x+y
+def subtract(x,y):
+    """this f subtracts"""
+    return x-y
+def add(x,y):
+    """this f multiply"""
+    return x*y
+def subtract(x,y):
+    """this f divides"""
+    return x/y
+
+    
+
