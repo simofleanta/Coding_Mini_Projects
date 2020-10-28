082200a449c013ae52280957d39a7d855680cb1f
simofleanta <simo.fleanta@gmail.com>
2020-10-28 15:06:32 +0100

    save

--- exercises/dojo.py
+++ exercises/dojo.py
@@ -6,25 +6,19 @@
 class Circle:
     def __init__(self, radius,area=0,circumference=0):
         self.radius=radius
+        self._circumference=circumference
         self._area=area
-        self.circumference=circumference
-
-    def get_area(self):
-        return self.get_area 
-
-    def get_circumference(self):
-        return self.get_area 
     
+    def get_area(self):
+        return self._area
     def set_area(self,a):
         self._area=a
-    
+    def get_circumference(self):
+        return self._circumference
     def set_circumference(self,a):
         self._circumference=a
+p=Circle(20)
 
-p = Circle(0.2) 
 
-# retrieving age using getter 
-print(p.get_area()) 
-print(p._area)
-print(p.get_circumference())
-print(p.circumference)
+ 
+
