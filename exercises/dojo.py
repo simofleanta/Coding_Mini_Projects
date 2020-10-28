f78ff5f024a556197d46373aad4187e703e5cf77
simofleanta <simo.fleanta@gmail.com>
2020-10-28 09:51:48 +0100

    save

---
+++ exercises/dojo.py
@@ -0,0 +1,14 @@
+
+
+class Student:
+    def __init__(self, name, roll):
+        self.name=name
+        self.roll=roll
+
+
+    def display(self):
+        print(self.name,self.roll)
+p=Student('Adrian', 2)
+
+
+
