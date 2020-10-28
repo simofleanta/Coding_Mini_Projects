d02b240d3b11075ee3ad6eccb75d61199508b657
simofleanta <simo.fleanta@gmail.com>
2020-10-28 13:52:00 +0100

    save

--- exercises/dojo.py
+++ exercises/dojo.py
@@ -4,10 +4,32 @@ class Student:
     def __init__(self, name, roll):
         self.name=name
         self.roll=roll
-
+    
+          
+    def display(self):
+        print(f"{self.name} and {self.roll}")
         
     def set_age(self, age):
-      return "{} age {}".format(self.name, self.roll)
+        self.set_age(age)
+    
+    def set_marks(self, marks):
+        self.set_marks(marks)
+
+    
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+"""return "{} age {}".format(self.name, self.roll)
 
 
     def set_marks(self, marks):
@@ -16,4 +38,6 @@ class Student:
     def display(self):
         print(f"{self.name} and {self.roll}, and {self.age} and {self.marks}")
 
-    
+p=Student('Adrian',2,36,10)
+p.display()"""
+
