f70e4e8c2f5925638759677acc2e2f89d1ed085a
simofleanta <simo.fleanta@gmail.com>
2020-10-28 09:17:19 +0100

    save

--- exercises/calc2.py
+++ exercises/calc2.py
@@ -2,3 +2,14 @@ num1= float(input("enter first number:"))
 operator=input("Enter operator:")
 num2=float(input("enter second number"))
 
+if operator=='+':
+    print(num1+num2)
+elif operator =='-':
+    print(num1-num2)
+elif operator =='/':
+    print(num1/num2)
+elif operator=='*':
+    print(num1*num2)
+else:
+    exit
+
