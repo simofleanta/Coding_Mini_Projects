c660e8ceab47149ab1f1fcdaafc115adfc30c138
simofleanta <simo.fleanta@gmail.com>
2020-10-28 13:36:36 +0100

    save

--- exercises/dojo.py
+++ exercises/dojo.py
@@ -5,17 +5,15 @@ class Student:
         self.name=name
         self.roll=roll
 
-
-    def display(self):
-        print(self.name,self.roll)
-        print(f"i am {self.name} and {self.roll}")#using formater
         
-        
-
     def set_age(self, age):
       return "{} age {}".format(self.name, self.roll)
-    
+
+
     def set_marks(self, marks):
         return "{} marks {}".format(self.name, self.roll,self.marks)
+
+    def display(self):
+        print(f"{self.name} and {self.roll}, and {self.age} and {self.marks}")
 
     
--- exercises/new_oop.py
+++ exercises/new_oop.py
@@ -59,18 +59,17 @@ class Student:
         self.name=name
         self.roll=roll
 
-    def display(self):
-        print(f"{self.name} and {self.roll}")
         
-        
-
     def set_age(self, age):
       return "{} age {}".format(self.name, self.roll)
 
 
     def set_marks(self, marks):
         return "{} marks {}".format(self.name, self.roll,self.marks)
-        
+
+    def display(self):
+        print(f"{self.name} and {self.roll}, and {self.age} and {self.marks}")
+
 
 
 
