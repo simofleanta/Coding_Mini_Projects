4f38b887209c0c5436c60fadf2bf9491bfd9327c
simofleanta <simo.fleanta@gmail.com>
2020-10-29 12:32:59 +0100

    add seat capacity to buss

--- exercises/new_oop.py
+++ exercises/new_oop.py
@@ -10,9 +10,6 @@
 # really tough, its all about breaking the problem down into smaller, logical steps.
 
 
-
-
-
 #####################
 # Exercise 2
 #####################
@@ -55,34 +52,9 @@ class Time:
         pass  # YOUR CODE GOES HERE, REMOVE PASS
 
 
-#####################
-# Exercise 5
-#####################
-
-# Define property that should have the same value for every class instance
-
-# Expected Output:
-# Color: White, Vehicle name: School Volvo, Speed: 180, Mileage: 12
-# Color: White, Vehicle name: Audi Q5, Speed: 240, Mileage: 18
-
-# Bonus:
-# Create a new Motorbike class that will have color blue
-
-# Use the following code for this exercise.
-class Vehicle:
-
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
 
 
-class Bus(Vehicle):
-    pass  # YOUR CODE GOES HERE, REMOVE PASS
 
-
-class Car(Vehicle):
-    pass  # YOUR CODE GOES HERE, REMOVE PASS
 
 
 #####################
--- exercises/vehicle_class.py
+++ exercises/vehicle_class.py
@@ -30,13 +30,14 @@ class Bus(Vehicle):
         self.name='Audi Q5'
         self.speed=240
         self.mileage=18
+        self.seat_capacity=50
         
     def print_bus(self):
         print(self.name, self.color,self.speed,self.mileage)
 
 B=Bus('Audi Q5', 240,18)
 
-print("Bus name: ", B.name, B.color, B.speed, B.mileage)
+print("Bus name: ", B.name, B.color, B.speed, B.mileage, B.seat_capacity)
 
 class Car(Vehicle):
     def __init__(self, name,max_speed, mileage):
