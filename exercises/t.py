964c68560e6ffe8410aa4556c976a009cad1b0bf
simofleanta <simo.fleanta@gmail.com>
2020-10-29 13:05:42 +0100

    save

---
+++ exercises/t.py
@@ -0,0 +1,14 @@
+#####################
+# Exercise 2
+#####################
+
+# Create a temperature class. Make two methods :
+# 1. convert_fahrenheit - It will take celsius and will print it into Fahrenheit.
+# 2. convert_celsius - It will take Fahrenheit and will convert it into Celsius.
+
+class Temperature:
+    def convert_fahrenheit(self, celsius):
+        pass  # YOUR CODE GOES HERE, REMOVE PASS
+
+    def convert_celsius(self, fahrenheit):
+        pass  # YOUR CODE GOES HERE, REMOVE PASS
