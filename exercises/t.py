fc924b7c6e0c619e6811e0e112bb6c83149c908f
simofleanta <simo.fleanta@gmail.com>
2020-10-29 13:55:12 +0100

    save

--- exercises/t.py
+++ exercises/t.py
@@ -3,28 +3,58 @@
 # 1. convert_fahrenheit - It will take celsius and will print it into Fahrenheit.
 # 2. convert_celsius - It will take Fahrenheit and will convert it into Celsius.
 
+
 class Temperature:
-    def __init__(self, fahrenheit,celsius):
-        self.fahrenheit=fahrenheit
+ 
+    def __init__(self, celsius, fahrenheit):
         self.celsius=celsius
-
-    def t(self):
-        print(self.fahrenheit,self.celsius)
-
-
+        self.fahrenheit=fahrenheit
+        
     def convert_fahrenheit(self, celsius=20):
-        return (celsius * 9/5) + 32
+        return((self.celsius * 9/5) + 32)
 
          
 
     def convert_celsius(self, fahrenheit=50):
-        return (fahrenheit - 32) * 5/9
+        return((self.fahrenheit - 32) * 5/9)
 
-
-    
-      
  
 
-p=Temperature(50,20)
-p.t()
+p=Temperature('celsius','fahrenheit')
 print("conversions :", p.convert_celsius,p.convert_fahrenheit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""class Temperature:
+    t=0
+    def __init__(self, temperature):
+        self.temperature=temperature     
+    
+
+
+    def convert_fahrenheit(self):
+        t=float((self.temperature * 9)/5 + 32)
+        return t
+
+         
+
+    def convert_celsius(self):
+        t=float((self.temperature - 32) * 5/9)
+        return t
+
+ 
+
+p=Temperature(50)
+print("conversions :", p.convert_celsius,p.convert_fahrenheit)"""
