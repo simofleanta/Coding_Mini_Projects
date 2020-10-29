5adf1e1f00805c8e13ffc64b30fcf6e836985a51
simofleanta <simo.fleanta@gmail.com>
2020-10-29 15:03:30 +0100

    save

--- exercises/new_oop.py
+++ exercises/new_oop.py
@@ -10,20 +10,7 @@
 # really tough, its all about breaking the problem down into smaller, logical steps.
 
 
-#####################
-# Exercise 2
-#####################
 
-# Create a temperature class. Make two methods :
-# 1. convert_fahrenheit - It will take celsius and will print it into Fahrenheit.
-# 2. convert_celsius - It will take Fahrenheit and will convert it into Celsius.
-
-class Temperature:
-    def convert_fahrenheit(self, celsius):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-    def convert_celsius(self, fahrenheit):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
 
 
 #####################
