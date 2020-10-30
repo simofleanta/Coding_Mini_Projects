646c9f66b0798d044edf66238125f62f9476cbf2
simofleanta <simo.fleanta@gmail.com>
2020-10-30 14:47:11 +0100

    save

---
+++ exercises/isin.py
@@ -0,0 +1,18 @@
+# Determine if School_bus is also an instance of the Vehicle class
+
+# Expected Output:
+# True
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+
+class Bus(Vehicle):
+    pass
+
+School_bus=("School Volvo", 22,50)
+print(isinstance(School_bus, Vehicle))
--- exercises/new_oop.py
+++ exercises/new_oop.py
@@ -13,30 +13,6 @@
 
 
 
-#####################
-# Exercise 4
-#####################
-
-# Create a Time class and initialize it with hours and minutes.
-# 1. Make a method add_time which should take two time object and add them.
-# Example: (2 hour and 50 min)+(1 hr and 20 min) is (4 hr and 10 min)
-# 2. Make a method display_time which should print the time.
-# 3. Make a method display_minute which should display the total minutes in the Time.
-# Example: (1 hr 2 min) should display 62 minute.
-
-class Time:
-
-    def __init__(self, hours, minutes):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-    def add_time(self, t1, t2):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-    def display_time(self):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-    def display_minute(self):
-        pass  # YOUR CODE GOES HERE, REMOVE PASS
 
 
 
