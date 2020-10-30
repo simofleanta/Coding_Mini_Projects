c8309dcc9e160d8e9f372ee30036413985807ce4
simofleanta <simo.fleanta@gmail.com>
2020-10-30 15:28:38 +0100

    save

--- exercises/new_oop.py
+++ exercises/new_oop.py
@@ -14,66 +14,8 @@
 
 
 
-
-
 #####################
-# Exercise 7
-#####################
-
-# Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is seating
-# capacity * 100. If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance charge.
-# So total fare for bus instance will become the final amount = total fare + 10% of the total fare.
-
-# Note: The bus seating capacity is 50. so the final fare amount should be 5550. You need to override the fare()
-# method of a Vehicle class in Bus class.
-
-# Expected Output:
-# Total Bus fare is: 5500.0
-
-# Use the following code for your parent Vehicle class. We need to access the parent class from inside a method of a
-# child class.
-
-class Vehicle:
-    def __init__(self, name, mileage, capacity):
-        self.name = name
-        self.mileage = mileage
-        self.capacity = capacity
-
-    def fare(self):
-        return self.capacity * 100
-
-
-class Bus(Vehicle):
-    pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-
-School_bus = Bus("School Volvo", 12, 50)
-print("Total Bus fare is:", School_bus.fare())
-
-
-#####################
-# Exercise 8
-#####################
-
-# Determine if School_bus is also an instance of the Vehicle class
-
-# Expected Output:
-# True
-
-
-class Vehicle:
-    def __init__(self, name, mileage, capacity):
-        self.name = name
-        self.mileage = mileage
-        self.capacity = capacity
-
-
-class Bus(Vehicle):
-    pass  # YOUR CODE GOES HERE, REMOVE PASS
-
-
-#####################
-# Exercise 8
+# Exercise 9
 #####################
 
 # Create a classes structure for the products E-mag https://www.emag.ro/
