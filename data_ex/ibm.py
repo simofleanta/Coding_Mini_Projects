a58c7925f1b7f5185b248a3cde4a24aa07bf517d
simofleanta <simo.fleanta@gmail.com>
2020-11-04 18:59:49 +0100

    save changes

---
+++ data_ex/ibm.py
@@ -0,0 +1,69 @@
+
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import plotly
+import statistics
+import stats
+
+#What is the correct mean and standard deviation of the quantity of pasta purchased by time unit by household?
+
+c=pd.read_csv('ibm_course.csv')
+#print(c.columns)
+df=DataFrame(c.head(100))
+print(df.head(100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
