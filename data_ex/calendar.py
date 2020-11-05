d296fa1b62dd8d75d29d8ab21059bcc6d73a3381
simofleanta <simo.fleanta@gmail.com>
2020-11-05 13:07:13 +0100

    save function calendar

--- data_ex/calendar.py
+++ data_ex/calendar.py
@@ -8,4 +8,15 @@ dd=int(input("enter day: "))
 print("show calendar")
 print(calendar.monthcalendar(yy,mm))
 
+#-------------------------------------------------
+def calendars(year,month):
+    year=2020
+    month=11
+    
+    return (calendar.calendar(year,month))
+
+calendar=calendars(2020,11)
+print(calendar)
+    
+
 
