1fdf497982b0f947f55014e256c4684c5ad1073d
simofleanta <simo.fleanta@gmail.com>
2020-11-05 08:46:12 +0100

    add day

--- data_ex/calendar.py
+++ data_ex/calendar.py
@@ -1,9 +1,11 @@
 import calendar
 yy=2020
 mm=11
+dd=5
 yy=int(input("enter year:"))
 mm=int(input("enter month: "))
-print("display calendar")
+dd=int(input("enter day: "))
+print("show calendar")
 print(calendar.monthcalendar(yy,mm))
 
 
