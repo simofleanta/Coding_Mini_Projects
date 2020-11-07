dd76bb3c15129ba20ffec5f3cb6b6b09ce7351a6
simofleanta <simo.fleanta@gmail.com>
2020-11-07 13:36:34 +0100

    s

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 def pattern(n):
     pyramid=2*n-2
-    for i in range(n,-1,-1):
-        for j in range (pyramid,0,-1):
+    for i in range(0,n):
+        for j in range(0, pyramid):
             print(end="")
         pyramid=pyramid-1
         for j in range( 0, i+1):
