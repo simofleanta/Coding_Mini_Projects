81600a4ce1458c728e370688c97da932be02df48
simofleanta <simo.fleanta@gmail.com>
2020-12-05 10:40:22 +0100

    save comms

--- passwort_gen.py
+++ passwort_gen.py
@@ -7,7 +7,11 @@ Caps="ABCD"
 No="012345"
 Sy=".;*/,[],]"
 
+#gather all chars
 all_c=low+Caps+No+Sy
+#set length
 length=16
+#join all chars randomly using random.sample
 password="".join(random.sample(all_c,length))
 print(password)
+
