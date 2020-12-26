e91b05f16a0ef95a3cb3175ea3588cb5ee7efbec
simofleanta <simo.fleanta@gmail.com>
2020-12-26 17:05:31 +0100

    added docstrings

--- Project_Portfolio/guess number simple fuction.py
+++ Project_Portfolio/guess number simple fuction.py
@@ -3,10 +3,13 @@ program"""
 import random
 
 def guess_it(x):
+    """ function creating a random number"""
     random_x=random.randint(1,x)
     guess=0 # so tht guess will never ==0, hence
     while guess_it != random_x: #while the function not coincide the random_x var, the loop will keep playing. It will break when function guess_it ==random(x)
+        #added guess in integer input so that the answer is an int. 
         guess=int(input(f'guess a number between 1 and {x}: '))
+        #guess would help creating the if clauses. 
         if guess < random_x:
             print('too low high, try again')
         elif guess > random_x:
@@ -14,6 +17,7 @@ def guess_it(x):
         else:
             print('Yup you got it {random_x}')
 
+#call function
 print(guess_it(20))
        
 
