0ac6d6b82ad68aaed3139e7933710d24739a4c9f
simofleanta <simo.fleanta@gmail.com>
2020-12-31 17:19:26 +0100

    s

---
+++ tic_toe_board.py
@@ -0,0 +1,34 @@
+import turtle
+
+board=turtle.Screen()
+t=turtle.Turtle()
+t.color("green")
+t.width()
+t.speed(4)
+t.shape('turtle')
+t.shapesize(0.5)
+
+for i in range(4):
+    t.forward(300)
+    t.left(90)
+
+#inner lines in the square
+t.penup()
+t.goto(0,100)
+t.pendown()
+t.forward(300)
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.forward(300)
+t.penup()
+t.goto(100,0)
+t.pendown()
+t.left(90)
+t.forward(300)
+t.penup()
+t.goto(200,0)
+t.pendown()
+t.forward(300)
+
+
