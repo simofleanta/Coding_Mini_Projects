55e265af8cc65db049697f0bcc7617ce5d96b7fe
simofleanta <simo.fleanta@gmail.com>
2020-12-31 18:21:54 +0100

    save

--- Project_Portfolio/race.py
+++ Project_Portfolio/race.py
@@ -4,13 +4,13 @@ from turtle import Turtle
 from random import randint
 #open window on screen
 window=turtle.Screen()
-window.title("Turtle race") 
+window.title("Happy New Year :)") 
 turtle.bgcolor("darkred")
 turtle.color("White")
 turtle.speed(0)
 turtle.penup()
 turtle.setpos(-140, 200)
-turtle.write("Race to 2021", font=("Verdana",30))
+turtle.write("Happy New Year", font=("Verdana",30))
 turtle.penup() 
 
 #2020 dirt
